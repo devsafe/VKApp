50101b818aa53c67ebf54039828e29c2eeb5d968
Boris Sobolev <rafinad@outlook.com>
2021-08-06 19:52:57 +0300

    add launch screen logo

--- VKApp/LoginFormController.swift
+++ VKApp/LoginFormController.swift
@@ -8,22 +8,16 @@
 import UIKit
 
 class LoginFormController: UIViewController {
-
+    @IBOutlet private var loginTextField: UITextField!
+    @IBOutlet private var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
     }
-    */
-
+    
 }
