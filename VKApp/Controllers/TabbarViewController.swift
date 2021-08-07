d440006e148eb9fcdaae66475710840405cb3885
Boris Sobolev <rafinad@outlook.com>
2021-08-07 14:33:14 +0300

    add tabbarviewcontroller

---
+++ VKApp/Controllers/TabbarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabbarViewController.swift
+//  VKApp
+//
+//  Created by Boris Sobolev on 07.08.2021.
+//
+
+import UIKit
+
+class TabbarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
