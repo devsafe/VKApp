6c15233b2df726811a0349de6c36b3c449f2ac24
Boris Sobolev <rafinad@outlook.com>
2021-08-16 19:25:34 +0300

    add comments and likes button and count labels

--- VKApp/Controllers/Photos/Cell/PhotosCollectionViewCell.swift
+++ VKApp/Controllers/Photos/Cell/PhotosCollectionViewCell.swift
@@ -12,8 +12,14 @@ class PhotosCollectionViewCell: UICollectionViewCell {
     static let identifier = "PhotosCollectionViewCell"
     
     @IBOutlet var imagePhotosCollectionCell: UIImageView!
-    @IBOutlet var imageLikePhotosCollectionCell: UIImageView!
     @IBOutlet var likeCountLabelCollectionCell: UILabel!
+    @IBOutlet var likeButtonOutlet: UIButton!
+    @IBOutlet var commentsButtonOutlet: UIButton!
+    @IBOutlet var commentsCountLabelOutlet: UILabel!
+    @IBAction func commentsButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func likeButtonPressed(_ sender: UIButton) {
+    }
     
     func configure(imageName: String?, likeCount: Int) {
         imagePhotosCollectionCell.image = UIImage(named: imageName ?? "logo-vk")
@@ -21,14 +27,17 @@ class PhotosCollectionViewCell: UICollectionViewCell {
         imagePhotosCollectionCell.layer.borderWidth = 1
         imagePhotosCollectionCell.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         let yourColor : UIColor = UIColor( red: 1, green: 1, blue: 1, alpha: 0.8 )
-        let yourColor2 : UIColor = UIColor( red: 1, green: 1, blue: 1, alpha: 0.5 )
+        //let yourColor2 : UIColor = UIColor( red: 1, green: 1, blue: 1, alpha: 0.5 )
         imagePhotosCollectionCell.layer.borderColor = yourColor.cgColor
         
         likeCountLabelCollectionCell.text = String(likeCount)
-        imageLikePhotosCollectionCell.image = UIImage(named: "heart")
-        let tintableImage = imageLikePhotosCollectionCell.image!.withRenderingMode(.alwaysTemplate)
-        tintableImage.withTintColor(.white)
-        imageLikePhotosCollectionCell.image = tintableImage
-        imageLikePhotosCollectionCell.tintColor = yourColor2
+       // imageLikePhotosCollectionCell.image = UIImage(named: "heart")
+        //let tintableImage = imageLikePhotosCollectionCell.image!.withRenderingMode(.alwaysTemplate)
+       // tintableImage.withTintColor(.white)
+       // imageLikePhotosCollectionCell.image = tintableImage
+        //imageLikePhotosCollectionCell.tintColor = yourColor2
+        commentsButtonOutlet.setImage(UIImage(systemName: "bubble.left"), for: .normal)
+        commentsButtonOutlet.titleLabel?.text = ""
+        likeButtonOutlet.setImage(UIImage(systemName: "heart"), for: .normal)
     }
 }
