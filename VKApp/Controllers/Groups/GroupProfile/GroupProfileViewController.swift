b6a3c664a8b989d77640a39f55c534643a0e0beb
Boris Sobolev <rafinad@outlook.com>
2021-08-25 22:55:08 +0300

    add follow button

--- VKApp/Controllers/Groups/GroupProfile/GroupProfileViewController.swift
+++ VKApp/Controllers/Groups/GroupProfile/GroupProfileViewController.swift
@@ -14,19 +14,33 @@ class GroupProfileViewController: UIViewController {
     @IBOutlet var groupControlView: UIView!
     @IBOutlet var logoGroupProfileOutlet: UIImageView!
     @IBOutlet var backGroungViewOutlet: UIView!
+    @IBOutlet var smallLogoGroupProfileOutlet: UIImageView!
+    @IBOutlet var fullGroupDescriptionLabel: UILabel!
+    @IBOutlet var groupSubscribersCountLabelOutlet: UILabel!
+    @IBOutlet var followButtonOutlet: UIButton!
+    @IBAction func followGroupButtonPressed(_ sender: UIButton) {
+        print("follow pressed")
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         logoGroupProfileOutlet.image = UIImage(named: groupFromOtherView.logo)
+        
         title = groupFromOtherView.name
         print(groupFromOtherView.name)
         let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.regular)
         let blurEffectView = UIVisualEffectView(effect: blurEffect)
-        blurEffectView.frame = view.bounds
+        blurEffectView.frame = logoGroupProfileOutlet.bounds
         blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        view.addSubview(blurEffectView)
-        backGroungViewOutlet.layer.cornerRadius = 8
+        logoGroupProfileOutlet.addSubview(blurEffectView)
+        smallLogoGroupProfileOutlet.image = UIImage(named: groupFromOtherView.logo)
+        followButtonOutlet.layer.cornerRadius = 8
+        followButtonOutlet.setTitle(isGroupInFav(groupName: groupFromOtherView.name) ? "Unfollow" : "Follow", for: .normal)
        // backGroungViewOutlet.alpha = 0.9
+    }
+    
+    func isGroupInFav(groupName: String) -> Bool {
+        (Storage.allUsers[Storage.userIdActiveSession].favGroups.firstIndex(where: { $0.name == groupName }) != nil) ? true : false
     }
 }
